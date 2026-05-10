83be87ffdfb31d8eff2ca4eeee25868455c4af7f
Mou Saha <mousaha695@gmail.com>
2026-05-10 20:09:17 +0200

    fix remaining sqlfluff violations - column order and trailing newlines

--- dbt/energy_dbt/models/staging/stg_energy_consumption.sql
+++ dbt/energy_dbt/models/staging/stg_energy_consumption.sql
@@ -13,9 +13,9 @@ renamed as (
         utc_timestamp as measured_at,
         country as country_code,
         consumption_mw,
+        loaded_at,
         date_trunc('day', utc_timestamp) as measured_date,
-        extract(hour from utc_timestamp) as hour_of_day,
-        loaded_at
+        extract(hour from utc_timestamp) as hour_of_day
     from source
 )
 
